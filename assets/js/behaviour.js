06566cfeec987fc9c0073284bbb6b77cf8484f4d
ErickMayorga <49161419+ErickMayorga@users.noreply.github.com>
2021-08-29 14:18:55 -0500

    filter

--- assets/js/behaviour.js
+++ assets/js/behaviour.js
@@ -1,4 +1,5 @@
-function crearFiltro(nombre){
+function crearFiltro(){
+    var nombre = "Erick";
     let li = document.createElement("li");
     li.setAttribute("data-filter",".filter-" + nombre);
     document.querySelector("#portfolio-flters").appendChild(li);
