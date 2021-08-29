25fc98840d57e6c2d4a307910de4471bc86ec9d3
ErickMayorga <49161419+ErickMayorga@users.noreply.github.com>
2021-08-29 14:53:46 -0500

    filters

--- assets/js/init.js
+++ assets/js/init.js
@@ -167,11 +167,40 @@ function crearDivRecuerdo(imagen,contenido,titulo1,titulo2,delay){
 
   }
 
+  function modalFilters(){
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+  }
+
   function init(){
     insertarFotos();
     insertarPoemas();
     insertarRecuerdos();
     crearDivRecuerdo("assets/img/jime.png","Contenido del recuerdo","PibuBear","Dibujito","100");
+    modalFilters();
   }
   
   window.onload = init;
